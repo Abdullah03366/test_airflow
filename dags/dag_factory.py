0e035246040974086d9a8e8b800e59f080264389
Abdullah03366 <74244825+Abdullah03366@users.noreply.github.com>
2025-05-12 12:06:49 +0200

    Create dag_factory.py

---
+++ dags/dag_factory.py
@@ -0,0 +1,35 @@
+import os, yaml
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+DEFAULT_ARGS = {
+    'owner': 'legacy',
+    'start_date': datetime(2025, 5, 12),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+    'depends_on_past': False,
+}
+
+BASE_SCRIPT_PATH = '/opt/airflow/legacy/bash'
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'jobs_config.yml')
+
+with open(CONFIG_FILE) as f:
+    jobs = yaml.safe_load(f)
+
+for job in jobs:
+    dag_id = f"legacy.{job['job_id']}_v1"
+    dag = DAG(
+        dag_id=dag_id,
+        default_args=DEFAULT_ARGS,
+        schedule_interval='@daily',
+        catchup=False,
+        tags=['legacy','bash']
+    )
+    BashOperator(
+        task_id=f"run_{job['job_id']}",
+        bash_command=f"bash {BASE_SCRIPT_PATH}/{job['script']}",
+        dag=dag,
+        do_xcom_push=False,
+    )
+    globals()[dag_id] = dag
