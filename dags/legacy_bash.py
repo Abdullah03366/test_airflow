cd95f375804f13c5358de62c14ff21932fc12669
Abdullah03366 <74244825+Abdullah03366@users.noreply.github.com>
2025-05-12 13:53:53 +0200

    Update legacy_bash.py

--- dags/legacy_bash.py
+++ dags/legacy_bash.py
@@ -1,7 +1,14 @@
-import os, yaml
+import os
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+
+SCRIPT_SUBDIR = "scripts"
+SCRIPT_NAME = "hello1.sh"
+
+DAG_FOLDER_TEMPLATE = '{{ dag_run.dag.folder }}' 
+
+FULL_SCRIPT_PATH_TEMPLATE = os.path.join(DAG_FOLDER_TEMPLATE, SCRIPT_SUBDIR, SCRIPT_NAME)
 
 default_args = {
     'owner': 'test',
@@ -11,16 +18,15 @@ default_args = {
 }
 
 with DAG(
-    dag_id='hello_world_legacy_bash_v1',
+    dag_id='hello_world_legacy_bash_v2',
     default_args=default_args,
     schedule_interval='@daily',
     catchup=False,
-    tags=['example', 'bash'],
+    tags=['example', 'bash', 'gitsync'],
 ) as dag:
-
     run_script = BashOperator(
-        task_id = "run_legacy_bash_script",
-        bash_command = "scripts/hello1.sh ",
+        task_id="run_legacy_bash_script",
+        bash_command=f"bash {FULL_SCRIPT_PATH_TEMPLATE}", 
     )
-
+    
     run_script
